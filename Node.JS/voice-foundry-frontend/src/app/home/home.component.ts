97759773dc6d366d586e4595a5daf5b6038fc23d
OneUp5hroom <Josh@alcanweb.com>
2021-05-05 09:22:00 -0500

    last 5 numbers is working on website homepage

--- Node.JS/voice-foundry-frontend/src/app/home/home.component.ts
+++ Node.JS/voice-foundry-frontend/src/app/home/home.component.ts
@@ -8,7 +8,9 @@ import { RestAPIService } from '../rest-api.service';
 })
 export class HomeComponent implements OnInit {
   public items: Array<any> = [];
-
+  public uniqueItems = new Set();
+  public isHidden: Array<boolean> = [];
+  public itemObjects: Array<any> = [];
   constructor(private rest: RestAPIService) { 
     console.log(this.items);
     
@@ -17,8 +19,29 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.rest.getTopFiveVanity().subscribe((returned: any) => {
       this.items = returned;
+      this.items.forEach(e => {
+        this.uniqueItems.add(e.parentNumber);
+      });
+      this.uniqueItems.forEach(n => {
+        // formatting number to make it look nicer.
+        let num = String(n).slice(0,3) + '-' + String(n).slice(3,6) + '-' + String(n).slice(6,10);
+        let output = {
+          parentNumber: String(n),
+          formattedNumber: num
+        }
+        console.log(output);
+        this.itemObjects.push(output);
+      });
+      for (let i=0; i < this.uniqueItems.size; i++) {
+        this.isHidden[i] = true;
+      }
     })
-    
   }
-
+  buttonClick(event: any): void {
+    if (this.isHidden[event.srcElement.id] === false) {
+      this.isHidden[event.srcElement.id] = true;
+    } else {
+        this.isHidden[event.srcElement.id] = false;
+    }
+  }
 }
